231b27a48ae7168b4a51b33574067f848d9ddeb5
zoranaism <lazareviczorana@gmail.com>
2020-04-24 12:23:33 +0200

    Seed files updated

--- routers/genre.js
+++ routers/genre.js
@@ -50,14 +50,12 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-// genre name, relations, name should be unique,
 // relations how many relations, that's how many rows, genreId and otherGenreId
-// auth/ YES!!
 // chek if there is already a genre with that name
 // dont allow relation duplication
 // one line decr limit character number FE and BE
 
-router.post("/", async (req, res, next) => {
+router.post("/", auth,  async (req, res, next) => {
   const { name, oneLineDescr, img, relations } = req.body;
 
   if (!name || !oneLineDescr || !img) {
@@ -66,12 +64,65 @@ router.post("/", async (req, res, next) => {
     });
   }
 
+  // const existingName = await Genre.findOne({ where: { name } });
+
+  // if (existingName) {
+  //   return res.status(409).send({
+  //     message: "This Genre entty conflicts with the existing one. Try with another Genre name.",
+  //   });
+  // }
+
   try {
     const genre = await Genre.create({
       name,
       oneLineDescr,
       img
     });
+
+    // const newRelations = relations.map((relation) => { 
+       
+    //     // const obj = {otherGenreId: relation, genreId: genre.id }
+
+    //     return obj
+    // })
+
+    // console.log("NEW RELATIONS", newRelations);
+    
+
+
+    // TRY I
+        //   var data = [
+        //     {
+        //         'cat_name':'fashion'
+        //     },
+        //     {
+        //         'cat_name':'food'
+        //     }
+        // ];
+        // orm.models.category.bulkCreate(data)
+        // })
+        
+    // TRY II
+        // const owners = [
+        //   {
+        //     name: "John",
+        //     role: "user"
+        //   },
+        //   {
+        //     name: "Sean",
+        //     role: "user"
+        //   }
+        // ];
+
+        // app.post('/owners/bulk', (req, res) => {
+        //   const ownerList = req.body.owners;
+        //   db.owners.bulkCreate(ownerList)
+        //     .then(newOwners => {
+        //       res.json(newOwners);
+        //     })
+        // });
+      
+    
 
     return res.status(201).json({ message: "New genre created.", genre });
   } catch (e) {
